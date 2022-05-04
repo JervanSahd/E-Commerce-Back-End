8cc44b2acee3a24471caf2f2eb4f923d3a23d39a
S.Snavely <info@stevesnavely.com>
2022-05-04 09:57:20 -0400

    Fixed belongToMany on models/index.js

--- models/index.js
+++ models/index.js
@@ -1,34 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
-Product.belongsTo(Category,{
-  foreignKey:"category_id",
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
   onDelete: "CASCADE",
-})
+});
 
 // Categories have many Products
 Categories.hasMany(Products, {
-  foreignKey:'category_id',
-
-})
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany (Tags,{
-  through:ProjectTag foreignKey:"product_id",
- 
+Product.belongsToMany(Tags, {
+  through: ProjectTag,
+  foreignKey: "product_id",
 }),
-// Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany (Products,{
-  through:ProjectTag foreignKey:"tag_id", 
-}),
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+  // Tags belongToMany Products (through ProductTag)
+  Tags.belongsToMany(Products, {
+    through: ProjectTag,
+    foreignKey: "tag_id",
+  }),
+  (module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  });
