6234a415f4ce8c968e5edcc79dc22ef34788df54
S.Snavely <info@stevesnavely.com>
2022-05-09 20:57:22 -0400

    Commit updates to routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,75 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
+// Import the model
+const Category = require("../../models/Category");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
-  // be sure to include its associated Products
-});
+  // be sure to include its associated Product
+  Category.findAll().then((categoryData) => 
+    res.json(categoryData))
+    .catch((err) => res.json(err))
+  });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  // Use Sequelize's `create()` method to add a row to the table
+  // Similar to `INSERT INTO` in plain SQL
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((newCategory) => {
+      // Send the newly created row as a JSON object
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
-});
+    // Calls the update method on the Category model
+    Category.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        category_name: req.body.category_name,
+      },
+      {
+        // Gets the category based on the id given in the request parameters
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then((updatedCategory) => {
+        // Sends the updated book as a json response
+        res.json(updatedCategory);
+      })
+      .catch((err) => res.json(err));
+  });
 
-router.delete('/:id', (req, res) => {
+
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll().then((productData) => 
+  res.json(productData))
+  .catch((err) => res.json(err))
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id).then((productData) => {
+    res.json(productData);
+  });
+
 });
 
 // create new product
@@ -91,6 +99,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => res.status(200).json(product))
+    .catch((err) => res.status(400).json(err));
+
 });
 
 module.exports = router;
